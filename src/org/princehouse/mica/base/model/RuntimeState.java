d7e310c676ecac51d1c8843e92d9f185e784674e
Josh Endries (jce54) <josh@endries.org>
2012-04-21 14:01:38 -0400

    Added data member.

--- src/org/princehouse/mica/base/model/RuntimeState.java
+++ src/org/princehouse/mica/base/model/RuntimeState.java
@@ -3,6 +3,7 @@ package org.princehouse.mica.base.model;
 
 import java.io.Serializable;
 import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.princehouse.mica.base.net.model.Address;
 
@@ -14,21 +15,43 @@ import org.princehouse.mica.base.net.model.Address;
  *
  */
 public class RuntimeState implements Serializable {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-
+	private Address address = null;
+	private Random random;
+	private final ConcurrentHashMap<String, Object> data = new ConcurrentHashMap<String, Object>();
+	
 	public RuntimeState() {
 		random = new Random();
 	}
 	
-	private Address address = null;
-	
 	public Address getAddress() {
 		return address;
 	}
+	
+	/**
+	 * Retrieve a reference to a data item.
+	 * 
+	 * @param key The key associated with the data item to be returned.
+	 * @return The data item associated with the specified key, or null if the key doesn't exist.
+	 */
+	public Object getData(String key) {
+		return data.get(key);
+	}
+	
+	public Random getRandom() {
+		return random;
+	}
+	
+	/**
+	 * Put the specified data item into the data map and associated it with the specified key.
+	 * 
+	 * @param key The key with which to associate the data item.
+	 * @param data The data to put into the data map.
+	 */
+	public Object putData(String key, Object data) {
+		return this.data.put(key, data);
+	}
+	
 	public void setAddress(Address address) {
 		// long tid = Thread.currentThread().getId();
 		// int hc = hashCode();
@@ -38,13 +61,9 @@ public class RuntimeState implements Serializable {
 			throw new RuntimeException("Address is read-only once it has been set"); 
 		this.address = address;
 	}
-	public Random getRandom() {
-		return random;
-	}
 	public void setRandom(Random random) {
 		this.random = random;
-	}
-	private Random random; 
+	} 
 	
 	public void update(RuntimeState update) {
 		if(!update.getAddress().equals(getAddress())) {
